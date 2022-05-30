2643a666967992b68055eedfe2fddd8a91d27955
Zet <58139125+Zet0x0@users.noreply.github.com>
2022-05-30 16:34:38 +0500

    Update main.cpp
    = clang-format

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -1,45 +1,55 @@
-#include <QRandomGenerator>
-#include <QElapsedTimer>
 #include <QApplication>
 #include <QColorDialog>
-#include <QImageReader>
-#include <QImageWriter>
+#include <QElapsedTimer>
 #include <QFileDialog>
 #include <QFontDialog>
+#include <QImageReader>
+#include <QImageWriter>
 #include <QMessageBox>
 #include <QPainter>
+#include <QRandomGenerator>
 
 QString convertMilliseconds(int milliseconds)
 {
     int hours = milliseconds / 3600000;
     int minutes = (milliseconds - hours * 3600000) / 60000;
 
-    return QString("%0:%1:%2").arg(QString::number(hours).rightJustified(2, '0'), QString::number(minutes).rightJustified(2, '0'), QString::number((milliseconds - hours * 3600000 - minutes * 60000) / 1000).rightJustified(2, '0'));
+    return QString("%0:%1:%2")
+        .arg(QString::number(hours).rightJustified(2, '0'),
+             QString::number(minutes).rightJustified(2, '0'),
+             QString::number((milliseconds - hours * 3600000 - minutes * 60000) / 1000)
+                 .rightJustified(2, '0'));
 }
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
-    qInstallMessageHandler([](QtMsgType, const QMessageLogContext &, const QString &message)
-                           {
-                               if (!message.startsWith("\n"))
-                               {
-                                   printf("%s\r", message.toStdString().c_str());
-                               }
-                               else
-                               {
-                                   printf(message.toStdString().c_str());
-                               } });
+    qInstallMessageHandler(
+        [](QtMsgType, const QMessageLogContext &, const QString &message)
+        {
+            if (!message.startsWith("\n"))
+            {
+                printf("%s\r", message.toStdString().c_str());
+            }
+            else
+            {
+                printf(message.toStdString().c_str());
+            }
+        });
 
-    QString imageFileName = QFileDialog::getOpenFileName(0, "", "", QString("Image File (*.%0)").arg(QImageReader::supportedImageFormats().join("; *.")));
+    QString imageFileName = QFileDialog::getOpenFileName(
+        0, "", "",
+        QString("Image File (*.%0)").arg(QImageReader::supportedImageFormats().join("; *.")));
 
     if (imageFileName.isEmpty())
     {
         return 1;
     }
 
-    QString resultFileName = QFileDialog::getSaveFileName(0, "", "", QString("Image File (*.%0)").arg(QImageWriter::supportedImageFormats().join("; *.")));
+    QString resultFileName = QFileDialog::getSaveFileName(
+        0, "", "",
+        QString("Image File (*.%0)").arg(QImageWriter::supportedImageFormats().join("; *.")));
 
     if (resultFileName.isEmpty())
     {
@@ -57,20 +67,22 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    if (QMessageBox::question(0, "", "Would you like to resize the image to make zeros and ones less visible? This could affect performance!") == QMessageBox::Yes)
+    if (QMessageBox::question(0, "",
+                              "Would you like to resize the image to make zeros and ones less "
+                              "visible? This could affect performance!") == QMessageBox::Yes)
     {
-        image = image.scaled(
-            image.width() * fontMetrics.boundingRect("0").width(),
-            image.height() * fontMetrics.boundingRect("0").height(),
-            Qt::KeepAspectRatio,
-            Qt::SmoothTransformation);
+        image = image.scaled(image.width() * fontMetrics.boundingRect("0").width(),
+                             image.height() * fontMetrics.boundingRect("0").height(),
+                             Qt::KeepAspectRatio, Qt::SmoothTransformation);
     }
 
     QImage result(image.size(), QImage::Format_ARGB32);
     QPainter painter(&result);
     QElapsedTimer timer;
 
-    if (QMessageBox::question(0, "", "Would you like to change the backround from transparent to something else?") == QMessageBox::Yes)
+    if (QMessageBox::question(
+            0, "", "Would you like to change the backround from transparent to something else?") ==
+        QMessageBox::Yes)
     {
         QColor fillColor(QColorDialog::getColor(Qt::black, 0, "", QColorDialog::ShowAlphaChannel));
 
@@ -83,9 +95,15 @@ int main(int argc, char **argv)
     painter.setFont(font);
     timer.start();
 
-    for (int y = 0; y < image.height(); y += ((fontMetrics.boundingRect("0").height() > fontMetrics.boundingRect("1").height()) ? fontMetrics.boundingRect("0").height() : fontMetrics.boundingRect("1").height()))
+    for (int y = 0; y < image.height();
+         y += ((fontMetrics.boundingRect("0").height() > fontMetrics.boundingRect("1").height())
+                   ? fontMetrics.boundingRect("0").height()
+                   : fontMetrics.boundingRect("1").height()))
     {
-        for (int x = 0; x < image.width(); x += ((fontMetrics.boundingRect("0").width() > fontMetrics.boundingRect("1").width()) ? fontMetrics.boundingRect("0").width() : fontMetrics.boundingRect("1").width()))
+        for (int x = 0; x < image.width();
+             x += ((fontMetrics.boundingRect("0").width() > fontMetrics.boundingRect("1").width())
+                       ? fontMetrics.boundingRect("0").width()
+                       : fontMetrics.boundingRect("1").width()))
         {
             QColor pixelColor = image.pixelColor(x, y);
 
@@ -98,10 +116,21 @@ int main(int argc, char **argv)
             painter.drawText(x, y, QString::number(QRandomGenerator::global()->bounded(2)));
 
             qDebug().noquote() << "Rendering..."
-                               << QString("%0%").arg(QString::number(double(y * image.width() + x) / double(image.width() * image.height()) * 100.0, 'f', 2).rightJustified(6, ' '))
+                               << QString("%0%").arg(
+                                      QString::number(double(y * image.width() + x) /
+                                                          double(image.width() * image.height()) *
+                                                          100.0,
+                                                      'f', 2)
+                                          .rightJustified(6, ' '))
                                << "|"
-                               << QString("X(%0/%1);").arg(QString::number(x).rightJustified(QString::number(image.width()).length(), ' '), QString::number(image.width()))
-                               << QString("Y(%0/%1);").arg(QString::number(y).rightJustified(QString::number(image.height()).length(), ' '), QString::number(image.height()))
+                               << QString("X(%0/%1);")
+                                      .arg(QString::number(x).rightJustified(
+                                               QString::number(image.width()).length(), ' '),
+                                           QString::number(image.width()))
+                               << QString("Y(%0/%1);")
+                                      .arg(QString::number(y).rightJustified(
+                                               QString::number(image.height()).length(), ' '),
+                                           QString::number(image.height()))
                                << "|"
                                << "Time elapsed:" << convertMilliseconds(timer.elapsed());
         }
